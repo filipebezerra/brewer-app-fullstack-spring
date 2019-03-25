fa6f9068eb45c189969c881d38753c29bac26243
Filipe Bezerra <filipebzerra@gmail.com>
2019-03-25 03:04:42 -0300

    Implementação inicial do sistema web brewer

---
+++ src/main/java/me/filipebezerra/brewer/config/init/AppInitializer.java
@@ -0,0 +1,25 @@
+package me.filipebezerra.brewer.config.init;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import me.filipebezerra.brewer.config.WebConfig;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { WebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+}
---
+++ src/main/java/me/filipebezerra/brewer/controller/CervejaController.java
@@ -0,0 +1,33 @@
+package me.filipebezerra.brewer.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import me.filipebezerra.brewer.model.Cerveja;
+
+@Controller
+public class CervejaController {
+
+	@RequestMapping("/cervejas/novo")
+	public String nova() {
+		return "cerveja/CadastroCerveja";
+	}
+	
+	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes redirectAttributes) {
+		if (result.hasErrors()) {
+			model.addAttribute("mensagem", "Há algo errado no formulário");
+			return "cerveja/CadastroCerveja";
+		}
+		
+		redirectAttributes.addFlashAttribute("mensagem", "Tudo certo no cadastro!");
+		return "redirect:/cervejas/novo";
+	}
+	
+}
