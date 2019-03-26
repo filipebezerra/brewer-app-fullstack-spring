fc73463324d1fe920fae868f8247f650fd6914b8
Filipe Bezerra <filipebzerra@gmail.com>
2019-03-26 01:39:33 -0300

    Primeiras validações usando Bean Validation

--- src/main/java/me/filipebezerra/brewer/model/Cerveja.java
+++ src/main/java/me/filipebezerra/brewer/model/Cerveja.java
@@ -1,13 +1,20 @@
 package me.filipebezerra.brewer.model;
 
+import javax.validation.constraints.Size;
+
 import org.hibernate.validator.constraints.NotBlank;
 
 public class Cerveja {
 
-	@NotBlank
+	@NotBlank(message = "SKU é obrigatório")
 	private String sku;
 
+	@NotBlank(message = "Nome é obrigatório")
 	private String nome;
+
+	@Size(min = 5, max = 50, message = "Descrição deve ter tamanho entre 5 e 50 caracteres")
+	@NotBlank(message = "Descrição é obrigatória")
+	private String descricao;
 
 	public String getSku() {
 		return sku;
@@ -23,6 +30,14 @@ public class Cerveja {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 	}
 
 }
